b79ea9e3785a5e21374851cb61756e62968d5f37
Leonardo Diniz <leonardodiber@gmail.com>
2024-02-10 14:16:41 -0300

    ✨ feat: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const messages = ["Sim!", "Claro!", "Com certeza!"]
+
+
+const getRandomMessage = (message_arr) => {
+    length = message_arr.length
+    index = Math.floor(Math.random() * length)
+    return message_arr[index]
+}
+
+function changeMessage(e) {
+    if (e.target.id === "change-message") {
+        const container = document.getElementById("message")
+        let message = getRandomMessage(messages)
+        if (container.innerText === message) message = getRandomMessage(messages)
+        container.innerText = message
+    }
+}
+
+window.onload = () => {
+    document.body.addEventListener('click', changeMessage)
+}
