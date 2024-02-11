7a94140e6c3d60123761002a989da6b3772c4c0d
Leonardo Diniz <leonardodiber@gmail.com>
2024-02-10 22:40:58 -0300

    🧹 chore(eslintrc): adds eslintrc

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-const messages = ["Sim!", "Claro!", "Com certeza!"]
-
+const messages = ["Sim!", "Claro!", "Com certeza!"];
 
 const getRandomMessage = (message_arr) => {
-  length = message_arr.length
-  index = Math.floor(Math.random() * length)
-  return message_arr[index]
-}
+  length = message_arr.length;
+  index = Math.floor(Math.random() * length);
+  return message_arr[index];
+};
 
 function changeMessage(e) {
   if (e.target.id === "change-message") {
-    const container = document.getElementById("message")
-    let message = container.innerText
+    const container = document.getElementById("message");
+    let message = container.innerText;
     while (container.innerText === message) {
-      message = getRandomMessage(messages)
+      message = getRandomMessage(messages);
     }
 
-    container.innerText = message
+    container.innerText = message;
   }
 }
 
 window.onload = () => {
-  document.body.addEventListener('click', changeMessage)
-}
+  document.body.addEventListener("click", changeMessage);
+};
